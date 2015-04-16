3df26e3ffba9945871768d49080bbfdb801c062f
Jens Gutermuth <jens@gutermuth-online.de>
2015-04-16 12:03:55 +0200

    first version

---
+++ check_mk_push_agent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import time
+
+import requests
+
+if __name__ == '__main__':
+    url = os.environ["URL"]
+    token = os.environ["TOKEN"]
+
+    while True:
+        try:
+            agent_data = subprocess.check_output(['/usr/bin/check_mk_agent'])
+            resp = requests.post(
+                '{}/push/{}'.format(url, token),
+                data=agent_data
+            )
+            if resp.status_code != 200:
+                raise RuntimeError('Server responded with '+str(resp))
+        except Exception as e:
+            print e
+        time.sleep(5)
